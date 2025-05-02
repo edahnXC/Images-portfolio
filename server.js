83371230ffa9434a32c3ffe2c943463cb6478748
Om Kumar <edahnXC@gmail.com>
2025-05-02 11:22:27 +0530

    public folder getting fetched

--- server.js
+++ server.js
@@ -6,17 +6,16 @@ const app = express();
 // Enable CORS
 app.use(cors());
 
-// Serve static files from root directory
-app.use(express.static(path.join(__dirname)));
+// Serve static files from the correct directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve all other routes with index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start server
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
 });
